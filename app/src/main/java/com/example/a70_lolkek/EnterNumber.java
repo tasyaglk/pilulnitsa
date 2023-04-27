0caf1da965660f0fbd8f0cecbf9b352ffdbe561a
Taisia Galkina <galkina.tasya@mail.ru>
2023-04-27 19:05:48 +0300

    Initial commit

---
+++ app/src/main/java/com/example/a70_lolkek/EnterNumber.java
@@ -0,0 +1,14 @@
+package com.example.a70_lolkek;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EnterNumber extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_number);
+    }
+}
---
+++ app/src/main/java/com/example/a70_lolkek/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.a70_lolkek;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private ImageView imageView2;
+    private TextView textView2;
+    private Button button2;
+    private Button button3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Находим все элементы управления по их ID
+        imageView = findViewById(R.id.imageView);
+        imageView2 = findViewById(R.id.imageView2);
+        textView2 = findViewById(R.id.textView2);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+
+//        // Задаем обработчики нажатия на кнопки
+//        button2.setOnClickListener(view -> {
+//            // Обработчик для кнопки "Номер телефона"
+//            // TODO: Действия при нажатии на кнопку "Номер телефона"
+//        });
+//
+        button3.setOnClickListener(view -> {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, EnterNumber.class);
+                startActivity(intent);
+            }
+
+        });
+    }
+}
