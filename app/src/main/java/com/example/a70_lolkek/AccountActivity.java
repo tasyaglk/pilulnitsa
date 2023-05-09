0e7e769b0af526a52250965bfc8b840c6d3e73b7
Taisia Galkina <galkina.tasya@mail.ru>
2023-05-09 18:43:42 +0300

    registrtion

--- app/src/main/java/com/example/a70_lolkek/AccountActivity.java
+++ app/src/main/java/com/example/a70_lolkek/AccountActivity.java
@@ -5,7 +5,9 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
