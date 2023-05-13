404e555cdc3c10aa35f63e80c2e6a7cbfaa2ca37
Taisia Galkina <galkina.tasya@mail.ru>
2023-05-13 16:44:38 +0300

    all in all

---
+++ app/src/main/java/com/example/a70_lolkek/LocalDateTypeAdapter.java
@@ -0,0 +1,31 @@
+package com.example.a70_lolkek;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+
+import java.lang.reflect.Type;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class LocalDateTypeAdapter implements JsonSerializer<LocalDate>, JsonDeserializer<LocalDate> {
+
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    @Override
+    public JsonElement serialize(final LocalDate date, final Type typeOfSrc,
+                                 final JsonSerializationContext context) {
+        return new JsonPrimitive(date.format(formatter));
+    }
+
+    @Override
+    public LocalDate deserialize(final JsonElement json, final Type typeOfT,
+                                 final JsonDeserializationContext context) throws JsonParseException {
+        String jsonString = json.toString();
+        return LocalDate.parse(jsonString.substring(1, jsonString.length() -1 ), formatter);
+    }
+}
